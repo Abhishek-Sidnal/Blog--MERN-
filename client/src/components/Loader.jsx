c655c6d2bb480846762c8fd6b877345877fd837c
Abhishek Sidnal <71709624+Abhishek-Sidnal@users.noreply.github.com>
2024-08-13 08:50:36 +0530

    .

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -3,7 +3,7 @@ import "./Loader.css";
 
 const Loader = () => {
   return (
-    <div className="h-full w-full flex items-center justify-center m-auto">
+    <div className="h-full min-h-screen w-full flex items-center justify-center m-auto">
       <span className="loader"></span>
     </div>
   );
