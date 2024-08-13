96f016a47da91fb97aa2af66050ac5edd22ea790
Abhishek Sidnal <71709624+Abhishek-Sidnal@users.noreply.github.com>
2024-08-13 08:29:04 +0530

    .

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import { FaBars, FaBlog, FaTimes } from "react-icons/fa";
-import { UserContext } from "../context/UserContext";
+import { UserContext } from "../context/userContext";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
