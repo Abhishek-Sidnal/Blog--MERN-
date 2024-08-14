6d1fb52ae835a2a49970e135b50e9b8d22ef8b92
Abhishek Sidnal <71709624+Abhishek-Sidnal@users.noreply.github.com>
2024-08-14 11:07:30 +0530

    .

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,32 +7,68 @@ const Footer = () => {
       <div className="container mx-auto px-4">
         <ul className="flex flex-wrap justify-center space-x-4 mb-4">
           <li>
-            <Link to="posts/categories/Agriculture" className="hover:text-blue-600">Agriculture</Link>
+            <Link
+              to="posts/categories/Agriculture"
+              className="hover:text-blue-600"
+            >
+              Agriculture
+            </Link>
           </li>
           <li>
-            <Link to="posts/categories/Business" className="hover:text-blue-600">Business</Link>
+            <Link
+              to="posts/categories/Business"
+              className="hover:text-blue-600"
+            >
+              Business
+            </Link>
           </li>
           <li>
-            <Link to="posts/categories/Education" className="hover:text-blue-600">Education</Link>
+            <Link
+              to="posts/categories/Education"
+              className="hover:text-blue-600"
+            >
+              Education
+            </Link>
           </li>
           <li>
-            <Link to="posts/categories/Entertainment" className="hover:text-blue-600">Entertainment</Link>
+            <Link
+              to="posts/categories/Entertainment"
+              className="hover:text-blue-600"
+            >
+              Entertainment
+            </Link>
           </li>
           <li>
-            <Link to="posts/categories/Art" className="hover:text-blue-600">Art</Link>
+            <Link to="posts/categories/Art" className="hover:text-blue-600">
+              Art
+            </Link>
           </li>
           <li>
-            <Link to="posts/categories/Investment" className="hover:text-blue-600">Investment</Link>
+            <Link
+              to="posts/categories/Investment"
+              className="hover:text-blue-600"
+            >
+              Investment
+            </Link>
           </li>
           <li>
-            <Link to="posts/categories/Uncategorized" className="hover:text-blue-600">Uncategorized</Link>
+            <Link
+              to="posts/categories/Uncategorized"
+              className="hover:text-blue-600"
+            >
+              Uncategorized
+            </Link>
           </li>
           <li>
-            <Link to="posts/categories/Weather" className="hover:text-blue-600">Weather</Link>
+            <Link to="posts/categories/Weather" className="hover:text-blue-600">
+              Weather
+            </Link>
           </li>
         </ul>
         <div className="text-center">
-          <small>&copy; {new Date().getFullYear()} CodingShala. All Rights Reserved.</small>
+          <small>
+            &copy; {new Date().getFullYear()} My Blog's. All Rights Reserved.
+          </small>
         </div>
       </div>
     </footer>
